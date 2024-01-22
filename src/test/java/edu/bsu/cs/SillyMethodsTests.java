b63ce8b2941a436ada3ca82c6dcd0f86f227531a
Lizzie Burton <ewburton@bsu.edu>
2024-01-22 16:52:13 -0500

    Initial Commit

---
+++ src/test/java/edu/bsu/cs/SillyMethodsTests.java
@@ -0,0 +1,37 @@
+package edu.bsu.cs;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SillyMethodsTests {
+    @Test
+    public void testDidGuess33Correctly() {
+        SillyMethods sillyMethods = new SillyMethods();
+        boolean answer = sillyMethods.didGuess33(33);
+        Assertions.assertTrue(answer);
+    }
+    @Test
+    public void testDidGuess45InCorrectly() {
+        SillyMethods sillyMethods = new SillyMethods();
+        boolean answer = sillyMethods.didGuess33(33);
+        Assertions.assertFalse(answer);
+    }
+    @Test
+    public void testCountTo9() {
+        SillyMethods sillyMethods = new SillyMethods();
+        boolean answer = sillyMethods.countTo(9);
+        Assertions.assertEquals("0 1 2 3 4 5 6 7 8 9", answer);
+    }
+    @Test
+    public void testCountTo11() {
+        SillyMethods sillyMethods = new SillyMethods();
+        boolean answer = sillyMethods.countTo(11);
+        Assertions.assertEquals("0 1 2 3 4 5 6 7 8 9 10 11", answer);
+    }
+
+
+
+
+
+
+}
